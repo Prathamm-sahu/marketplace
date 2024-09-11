17f8c4ae866a3f872e701c9541a7e032b925b3dc
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-11 22:23:00 +0530

    Created helpful utilities function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,76 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const categories = [
+  {
+    title: "Collectibles & Art",
+    href: "?category=collectiblesAndArt",
+    description: "NFTs, trading cards and more",
+  },
+  {
+    title: "Home & Garden",
+    href: "?category=homeAndGarden",
+    description: "Furniture, Kitchenware and more",
+  },
+  {
+    title: "Toys and Games",
+    href: "?category=toysAndGames",
+    description: "Board games, video games and more",
+  },
+  {
+    title: "Electronics",
+    href: "?category=electronics",
+    description: "Computer phones and more",
+  },
+  {
+    title: "Jewellry & Accessories",
+    href: "?category=jewelryAndAccessories",
+    description: "Watches, rings and more",
+  },
+];
+
+export enum categoryEnumValues {
+  collectiblesAndArt = "collectiblesAndArt",
+  electronics = "electronics",
+  homeAndGarden = "homeAndGarden",
+  jewelryAndAccessories = "jewelryAndAccessories",
+  toysAndGames = "toysAndGames"
+}
+
+export const convertStrToEnum = (convertingStr: string) => {
+  switch (convertingStr) {
+    case "collectiblesAndArt":
+        return categoryEnumValues.collectiblesAndArt;
+    case "electronics":
+        return categoryEnumValues.electronics;
+    case "homeAndGarden":
+        return categoryEnumValues.homeAndGarden;
+    case "jewelryAndAccessories":
+        return categoryEnumValues.jewelryAndAccessories;
+    case "toysAndGames":
+        return categoryEnumValues.toysAndGames;
+    default:
+        return categoryEnumValues.electronics;;
+}
+
+
+export type Sort = {
+  display: string;
+  value: string;
+};
+
+export const SORTS: Sort[] = [
+  { display: "Newest", value: "newest" },
+  { display: "Oldest", value: "oldest" },
+  { display: "Price: Low to High", value: "priceAsc" },
+  { display: "Price: High to Low", value: "priceDesc" },
+];
+
+export const valueToSort = (value: string): Sort | null => {
+  const sort = SORTS.find((sort) => sort.value === value);
+  if (!sort) {
+    return null;
+  }
+  return sort;
+}
