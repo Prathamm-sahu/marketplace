f07608ff9abdb7c0322f62f115fac09cc7f06c54
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-12 18:44:46 +0530

    added styles to item card

--- src/components/ItemCard.tsx
+++ src/components/ItemCard.tsx
@@ -3,7 +3,9 @@
 import { FC, useState } from 'react'
 import { Card, CardContent, CardFooter, CardHeader, CardTitle } from './ui/card'
 import { ItemType } from '@/lib/validators/item'
+import Image from 'next/image'
 import { Star } from 'lucide-react'
+import Link from 'next/link'
 
 interface ItemCardProps {
   item: ItemType
@@ -13,19 +15,20 @@ const ItemCard: FC<ItemCardProps> = ({ item }) => {
   const [isWatched, setIsWatched] = useState(false)
 
   return (
-    <>
-      <Card>
-        <CardHeader>
+    <Link href={`/item/${item.ownerId}/${item.id}`}>
+      <Card className='hover:scale-105 transform transition duration-300 '>
+        <CardHeader className='py-0'>
           <CardTitle>
             {/* Insert the image */}
+            <Image src={item.image} alt='ItemImage' width={240} height={240} className="rounded-t-md object-cover w-[240px] h-[240px]" />
           </CardTitle>
         </CardHeader>
-        <CardContent>
-          <div>{item.title}</div>
+        <CardContent className='py-4'>
+          <div className='hover:underline text-blue-500 font-medium hover:cursor-pointer'>{item.title}</div>
         </CardContent>
         <CardFooter>
-          <div>
-            <p>${item.price}</p>
+          <div className='flex justify-between items-center w-full gap-2'>
+            <p className="text-xl font-medium">${item.price}</p>
             {isWatched ? (
               <Star 
                 className='text-blue-500'
@@ -42,7 +45,7 @@ const ItemCard: FC<ItemCardProps> = ({ item }) => {
           </div>
         </CardFooter>
       </Card>
-    </>
+    </Link>
   )
 }
 
