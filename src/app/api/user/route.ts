2757a9520f98059a3b662dec4130c6d8d4335174
Pratham Sahu <prathamsahu051@gmail.com>
2024-09-13 23:30:46 +0530

    Created route to fetch the user

---
+++ src/app/api/user/route.ts
@@ -0,0 +1,39 @@
+import { getAuthSession } from "@/lib/auth";
+import { db } from "@/lib/db";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function GET(req: NextRequest) {
+  try {
+    const session = await getAuthSession()
+
+    if(!session?.user) {
+      return new Response("Unauthenticated", { status: 401 })
+    }
+
+    const userId = req.nextUrl.searchParams.get("userId")
+
+    if(!userId) {
+      return new Response("Invalid userId", { status: 400 })
+    }
+
+    const user = await db.user.findFirst({
+      where: {
+        id: userId
+      },
+      include: {
+        itemPurchased: true,
+        itemSelled: true,
+        items: true,
+        itemRatings: true,
+      }
+    })
+
+    if(!user) {
+      return new Response("User not found", { status: 404 })
+    }
+
+    return NextResponse.json(user)
+  } catch (error) {
+    return new Response("Something went wrong", { status: 500 })
+  }
+}
